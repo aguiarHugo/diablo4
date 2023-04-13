72c1638da3b7423e9e0c4f1018f3d7f79d5782d9
aguiarHugo <aguiarhugofx@gmail.com>
2023-04-13 10:17:16 -0300

    Styled

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,62 +1,123 @@
 import { useState } from "react";
-import Navbar from '../components/Navbar'
 import { motion } from "framer-motion";
 
-import lilith from '../assets/lilith-no-bg.png'
+import Navbar from '../components/Navbar';
 import SocialMedia from "../components/SocialMedia";
+import useMediaQuery from "../hooks/useMediaQuery";
+
+import lilith from '../assets/lilith-no-bg.png'
 
 const About = () => {
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+
   const [selectedPage, setSelectedPage] = useState('about')
   return (
-    <>
+    <section>
       <Navbar
         selectedPage={selectedPage}
         setSelectedPage ={setSelectedPage}
       />
-      <motion.div
-       initial="hidden"
-       whileInView="visible"
-       viewport={{ once: true, amount: 0.3}}
-       transition={{ duration: 0.7 }}
-       variants={{ 
-         hidden: { opacity:0, x:-60 },
-         visible: { opacity:1, x:0 }
-       }} 
-        className='section-wrapper'>
+      <div className='section-wrapper flex flex-col md:flex-row'>
+        { isAboveMediumScreens ? (
+          <>
+            <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.3}}
+            transition={{ duration: 0.7 }}
+            variants={{ 
+              hidden: { opacity:0, x:-60 },
+              visible: { opacity:1, x:0 }
+            }} 
+              className="flex flex-col gap-3"
+            >
               <h2 className="text-heading">
-                Who are we?
+                D4Fan
               </h2> 
-          <div class="grid grid-cols-2 grid-flow-row grid-auto-flow">
-            <div class="row-start-1 row-end-1">
               <p>
                 Welcome to our <span className="text-red-500 font-semibold">Diablo 4 game blog!</span> As avid gamers and fans of the
                 Diablo series, we are excited to share our insights, news, and
                 updates about Diablo 4 with fellow players like you.
               </p>
-            </div>
-            <div class="col-start-1 col-end-1">
               <p>
                 Our blog covers <span className="text-red-500 font-semibold">all aspects of Diablo 4, including gameplay, story,
                 characters, classes, items, and more.</span> We strive to provide
                 comprehensive and up-to-date information about the game, along with
                 our own analysis and opinions.
               </p>
-            </div>
-            <div class="col-start-1 col-end-1">
               <p>
                 Our team of experienced players is passionate about the Diablo
                 franchise and committed to delivering accurate and engaging content.
                 Whether you are a seasoned Diablo player or new to the series, our
-                <span className="text-red-500 font-semibold"> blog aims to be a valuable resource for all things Diablo 4.</span>
+                <span className="text-red-500 font-semibold"> blog aims to be a valuable resource for all things 
+                Diablo 4.</span>
               </p>
-            </div>
-            <div class="text-center row-start-1 row-end-4">
-              <img  className="h-[300px]" src={lilith} alt="" />
-            </div>
+              <SocialMedia />
+            </motion.div>
+          <div>
+            <motion.img
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.3}}
+              transition={{ ease: "easeOut", duration: 2 }}
+              variants={{ 
+                hidden: { opacity:0 },
+                visible: { opacity:1 }
+              }}
+              src={lilith} alt="Lilith" />
           </div>
-          <SocialMedia />
-        </motion.div>
-    </>
+          </>
+           ) : (
+          <>
+            <div>
+              <motion.img
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.3}}
+                transition={{ ease: "easeOut", duration: 2 }}
+                variants={{ 
+                  hidden: { opacity:0 },
+                  visible: { opacity:1 }
+                }}
+                src={lilith} alt="Lilith" />
+            </div>
+            <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.3}}
+            transition={{ duration: 0.7 }}
+            variants={{ 
+              hidden: { opacity:0, x:-60 },
+              visible: { opacity:1, x:0 }
+            }} 
+            className="flex flex-col gap-3">
+            <h2 className="text-heading">
+              D4Fan
+            </h2> 
+            <p>
+              Welcome to our <span className="text-red-500 font-semibold">Diablo 4 game blog!</span> As avid gamers and fans of the
+              Diablo series, we are excited to share our insights, news, and
+              updates about Diablo 4 with fellow players like you.
+            </p>
+            <p>
+              Our blog covers <span className="text-red-500 font-semibold">all aspects of Diablo 4, including gameplay, story,
+              characters, classes, items, and more.</span> We strive to provide
+              comprehensive and up-to-date information about the game, along with
+              our own analysis and opinions.
+            </p>
+            <p>
+              Our team of experienced players is passionate about the Diablo
+              franchise and committed to delivering accurate and engaging content.
+              Whether you are a seasoned Diablo player or new to the series, our
+              <span className="text-red-500 font-semibold"> blog aims to be a valuable resource for all things 
+              Diablo 4.</span>
+            </p>
+            <SocialMedia />
+          </motion.div>
+        </>
+        )} 
+        </div>
+    </section>
   )
 }
 
