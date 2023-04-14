7927557ebcb0427d4d11c2f65ee15b923366c804
aguiarHugo <aguiarhugofx@gmail.com>
2023-04-14 15:09:35 -0300

    New Classes and Footer added

--- src/pages/Classes.jsx
+++ src/pages/Classes.jsx
@@ -5,9 +5,12 @@ import Navbar from '../components/Navbar';
 import SocialMedia from "../components/SocialMedia";
 import useMediaQuery from "../hooks/useMediaQuery";
 
-import lilith from '../assets/lilith-no-bg.png'
-import Barbarian from "../components/Barbarian";
-import Sorceress from "../components/Sorceress";
+import Barbarian from "../components/GameClasses/Barbarian";
+import Sorceress from "../components/GameClasses/Sorceress";
+import Druid from "../components/GameClasses/Druid";
+import Rogue from "../components/GameClasses/Rogue";
+import Necromancer from "../components/GameClasses/Necromancer";
+import Footer from "../components/Footer";
 
 const Classes = () => {
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
@@ -19,8 +22,15 @@ const Classes = () => {
         selectedPage={selectedPage}
         setSelectedPage ={setSelectedPage}
       />
+      <h1 className="mt-40 text-center text-heading">Classes</h1>
+      <p className="text-center">Diablo 4 is an exciting new game that will feature five classes upon its release. These classes are:</p>
       <Barbarian />
       <Sorceress />
+      <Druid />
+      <Rogue />
+      <Necromancer />
+
+      <Footer />
     </section>
   )
 }
