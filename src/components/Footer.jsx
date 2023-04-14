5ef76af81c77d9366bd3083fdcc2b2dad7a6e545
aguiarHugo <aguiarhugofx@gmail.com>
2023-04-14 15:09:11 -0300

    Created and Styled

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <section>
+      <div className='h-16 bg-red-900 flex justify-center items-center'>
+        <p>Developed by <a href="https://aguiarhugo.netlify.app/" target='__blank' className='font-bold'>Hugo Aguiar</a></p>
+      </div>
+    </section>
+  )
+}
+
+export default Footer
