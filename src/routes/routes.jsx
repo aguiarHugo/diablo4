d86365fb4e1587797261d336a13369f10dba6020
aguiarHugo <aguiarhugofx@gmail.com>
2023-04-14 09:54:16 -0300

    Classes routes

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 
 import Home from "../pages/Home";
 import About from "../pages/About";
+import Classes from "../pages/Classes";
 
 export const router = createBrowserRouter([
   {
@@ -15,5 +16,9 @@ export const router = createBrowserRouter([
   {
     path: '/about',
     element: <About />
+  },
+  {
+    path: '/classes',
+    element: <Classes />
   }
 ])
