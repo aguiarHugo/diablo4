991a1fa5763d80a5fa18ec932776bae2cc878f71
aguiarHugo <aguiarhugofx@gmail.com>
2023-04-15 08:13:37 -0300

    News routes

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
 import About from "../pages/About";
 import Classes from "../pages/Classes";
+import News from "../pages/News";
 
 export const router = createBrowserRouter([
   {
@@ -20,5 +21,9 @@ export const router = createBrowserRouter([
   {
     path: '/classes',
     element: <Classes />
+  },
+  {
+  path: '/news',
+  element: <News />
   }
 ])
